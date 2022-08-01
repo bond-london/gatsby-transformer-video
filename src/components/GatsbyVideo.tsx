2479bf574b1faef73ee424bdf47fc4fd5742f3cf
nrandell <nick@nickrandell.com>
2022-08-01 14:41:06 +0100

    fix: update video props to allow no poster

--- src/components/GatsbyVideo.tsx
+++ src/components/GatsbyVideo.tsx
@@ -12,20 +12,21 @@ export function getGatsbyVideo(
 export const GatsbyVideo: React.FC<
   {
     videoData: GatsbyTransformedVideo;
+    noPoster?: boolean;
   } & React.DetailedHTMLProps<
     React.VideoHTMLAttributes<HTMLVideoElement>,
     HTMLVideoElement
   >
 > = (allProps) => {
-  const { videoData, muted, ...otherProps } = allProps;
+  const { videoData, noPoster, muted, ...otherProps } = allProps;
   return (
     // eslint-disable-next-line jsx-a11y/media-has-caption
     <video
-      width={videoData.width}
-      height={videoData.height}
-      poster={videoData.poster}
       muted={!videoData.hasAudio || muted}
       {...otherProps}
+      width={videoData.width}
+      height={videoData.height}
+      poster={noPoster ? undefined : videoData.poster}
     >
       <source type="video/webm" src={videoData.webm} />
       <source type="video/mp4" src={videoData.mp4} />
